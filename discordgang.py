23c24e242fd1e96d49ba147b4c5a3aaa24c34c43
Itzdvbravo <47894997+Itzdvbravo@users.noreply.github.com>
2019-04-14 15:52:07 +0530

    Update discordgang.py

--- discordgang.py
+++ discordgang.py
@@ -7,4 +7,10 @@ import os
 bot = discord.Client()
 bot = commands.Bot(command_prefix = '//')
 
+@bot.event
+async def on_ready():
+    game = discord.Activity(name="GodsDevil Network", type=discord.ActivityType.listening)
+    await bot.change_presence(status=discord.Status.dnd, activity=game)
+
+
 bot.run(os.getenv('TOKEN'))
