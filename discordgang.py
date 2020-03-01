31c2e717b1ce3d448c042507669679072de4c36c
Itzdvbravo <47894997+Itzdvbravo@users.noreply.github.com>
2020-03-01 21:55:28 +0530

    Update discordgang.py

--- discordgang.py
+++ discordgang.py
@@ -1,4 +1,4 @@
-import discor
+import discord
 from discord.ext import commands
 from discord import User
 from discord import Game
@@ -23,12 +23,32 @@ async def on_ready():
     game = discord.Activity(name="Itzdvbravo's Server", type=discord.ActivityType.listening)
     await bot.change_presence(status=discord.Status.dnd, activity=game)
 
+async def background_task():
+    await bot.wait_until_ready()
+    channel = bot.get_channel(683711122808766524)
+    while not bot.is_closed():
+        data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UC0DdJlY_b6ySlOp3YtDjvxA&key=AIzaSyCsfglSpz_K17iqA_ezeA5oD01pmhSerZ0").read()
+        subs = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
+        vid = json.loads(data)["items"][0]["statistics"]["videoCount"]
+        veiw = json.loads(data)["items"][0]["statistics"]["viewCount"]
+        embed=discord.Embed(title="Click to view my channel", url="https://www.youtube.com/channel/UC0DdJlY_b6ySlOp3YtDjvxA")
+        embed.set_author(name="Itzdvbravo's Youtube Live Status", icon_url="https://cdn.discordapp.com/avatars/533894799577841665/d44545c7c57ef5bde8ffa6a35c75e269.png?size=256")
+        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/683648447903039500/683700959473565726/minecraft-1469255_1920.png")
+        embed.add_field(name="Subsribers", value="{}".format(int(subs)), inline=True)
+        embed.add_field(name="Veiws", value="{}".format(int(veiw)), inline=True)
+        embed.add_field(name="Total Videos", value="{}".format(int(vid)), inline=True)
+        embed.set_footer(text="*Updated every 40 seconds*")
+        pong = await channel.send(embed=embed)
+        await asyncio.sleep(40)
+        await pong.edit(embed=embed)
+        await pong.delete()    
+    
 @bot.command()
 async def check(ctx, arg=None):
     data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UC0DdJlY_b6ySlOp3YtDjvxA&key=AIzaSyCsfglSpz_K17iqA_ezeA5oD01pmhSerZ0").read()
     subs = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
     vid = json.loads(data)["items"][0]["statistics"]["videoCount"]
-    veiw = json.loads(data)["items"][0]["statistics"]["viewCount"]
+    view = json.loads(data)["items"][0]["statistics"]["viewCount"]
     if arg == None:
         await ctx.send("Please use `;check <option>`, options are *subs* and *videocount/vc* and *veiws*")
     elif arg == 'subs':
@@ -37,9 +57,10 @@ async def check(ctx, arg=None):
         await ctx.send("Itzdvbravo has uploaded {} videos till now".format(int(vid)))
     elif arg == 'vc':
         await ctx.send("Itzdvbravo has uploaded {} videos till now".format(int(vid)))
-    elif arg == 'veiws':
-        await ctx.send("Itzdvbravo, in total he has {} veiws, in all his videos".format(int(veiw)))
+    elif arg == 'views':
+        await ctx.send("Itzdvbravo, in total he has {} views, in all his videos".format(int(veiw)))
     else:
         await ctx.send("Please use `;check <option>`, options are *subs* and *videocount/vc* and *veiws*")    
 
+bot.loop.create_task(background_task())        
 bot.run(os.getenv('TOKEN'))
