e197aef836f0d4f7e46947ac24e41a65f2fad743
Itzdvbravo <47894997+Itzdvbravo@users.noreply.github.com>
2020-03-04 12:20:17 +0530

    Update discordgang.py

--- discordgang.py
+++ discordgang.py
@@ -31,12 +31,12 @@ async def background_task():
         data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UC0DdJlY_b6ySlOp3YtDjvxA&key=AIzaSyCsfglSpz_K17iqA_ezeA5oD01pmhSerZ0").read()
         subs = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
         vid = json.loads(data)["items"][0]["statistics"]["videoCount"]
-        veiw = json.loads(data)["items"][0]["statistics"]["viewCount"]
+        view = json.loads(data)["items"][0]["statistics"]["viewCount"]
         embed=discord.Embed(title="Click to view my channel", url="https://www.youtube.com/channel/UC0DdJlY_b6ySlOp3YtDjvxA")
         embed.set_author(name="Itzdvbravo's Youtube Live Status", icon_url="https://cdn.discordapp.com/avatars/533894799577841665/d44545c7c57ef5bde8ffa6a35c75e269.png?size=256")
         embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/683648447903039500/683700959473565726/minecraft-1469255_1920.png")
         embed.add_field(name="Subsribers", value="{}".format(int(subs)), inline=True)
-        embed.add_field(name="Total Veiws", value="{}".format(int(veiw)), inline=True)
+        embed.add_field(name="Total Veiws", value="{}".format(int(view)), inline=True)
         embed.add_field(name="Total Videos", value="{}".format(int(vid)), inline=True)
         embed.set_footer(text="*Updates every 5 minutes*")
         await pong.edit(embed=embed)
